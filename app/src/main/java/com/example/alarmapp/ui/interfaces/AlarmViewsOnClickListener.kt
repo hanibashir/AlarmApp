c9ec438a15d6abe6802b3f1aa3071794df2079d3
hbalhassan <hbalhassan@gmail.com>
2022-08-17 17:57:45 +0200

    updating sdk 32

--- app/src/main/java/com/example/alarmapp/ui/interfaces/AlarmViewsOnClickListener.kt
+++
@@ -1,8 +0,0 @@
-package com.example.alarmapp.ui.interfaces
-
-import android.view.View
-
-interface AlarmViewsOnClickListener {
-    fun onSwitchToggle(position: Int)
-    fun onItemButtonClicked(btn: View, position: Int)
-}
